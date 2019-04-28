c8a8d79834f930badabece4fb9a9d6978f397b2d
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2019-04-28 11:22:10 -0400

    Initial commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public ValuesController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _dataContext.Values.ToListAsync();
+            return Ok(values);           
+        }
+
+
+         [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _dataContext.Values.FirstOrDefaultAsync(x => x.Id == id);
+            if(value == null) return NotFound();
+            return Ok(value);           
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Data/WorldWideContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatingApp.Data
+{
+//     public partial class WorldWideContext : DbContext
+//     {
+//         public WorldWideContext()
+//         {
+//         }
+
+//         public WorldWideContext(DbContextOptions<WorldWideContext> options)
+//             : base(options)
+//         {
+//         }
+
+//         public virtual DbSet<Blog> Blog { get; set; }
+//         public virtual DbSet<Post> Post { get; set; }
+
+//         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//         {
+//             if (!optionsBuilder.IsConfigured)
+//             {
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                 optionsBuilder.UseSqlServer("Server=EMMANUEL-NF-PC\\SQLEXPRESS;Database=Blogging;Integrated Security=true;");
+//             }
+//         }
+
+//         protected override void OnModelCreating(ModelBuilder modelBuilder)
+//         {
+//             modelBuilder.Entity<Blog>(entity =>
+//             {
+//                 entity.Property(e => e.Url).IsRequired();
+//             });
+
+//             modelBuilder.Entity<Post>(entity =>
+//             {
+//                 entity.HasOne(d => d.Blog)
+//                     .WithMany(p => p.Post)
+//                     .HasForeignKey(d => d.BlogId);
+//             });
+//         }
+//     }
+}
